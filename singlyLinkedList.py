e9178f2e6f19f680a7386ba42eaf58ad526e31e5
Sanidhya Rajguru <84410647+Sanidhya572@users.noreply.github.com>
2023-03-30 11:25:30 +0530

    Add files via upload

---
+++ singlyLinkedList.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            self.tail.next=new_node
+            self.tail=new_node
+        self.length += 1
+        return True
+
+
+my_linked_list = LinkedList(10)
+my_linked_list.append(3)
+my_linked_list.append(8)
+my_linked_list.append(40)
+my_linked_list.append(45)
+my_linked_list.append(7)
+my_linked_list.print_list()
+
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length)
+
+
+
+
+
+
+
+
+
+
